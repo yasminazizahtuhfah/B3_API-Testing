26acb41908f71cf37f6571c8a0cdd4ac69f56173
yasminazizahtuhfah <yasmin.azizah.tif421@polban.ac.id>
2024-04-29 07:34:23 +0700

    first commit

---
+++ src/test/java/test.java
@@ -0,0 +1,11 @@
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+
+public class test {
+    public static void main(String[] args) {
+        RestAssured.baseURI= "";
+        RequestSpecification req;
+
+    }
+
+}
