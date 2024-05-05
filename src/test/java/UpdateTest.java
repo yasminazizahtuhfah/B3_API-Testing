c33b25bcd8d7a961a8371c00fbe81a3eb0f26e76
yasminazizahtuhfah <yasmin.azizah.tif421@polban.ac.id>
2024-05-05 23:01:50 +0700

    update UpdateTest.java

--- src/test/java/UpdateTest.java
+++ src/test/java/UpdateTest.java
@@ -1,5 +1,9 @@
+import ch.qos.logback.core.net.SyslogOutputStream;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -7,51 +11,196 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class UpdateTest {
-
-    private String baseUrl = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
-    private String validAppId = "662ba92ffc6498c14d22ae13";
-
-    @BeforeClass
-    public void setUp() {
-        // Set base URL
-        RestAssured.baseURI = baseUrl;
-    }
-
     //TC-03-06-Melakukan operasi update pada field title dengan data valid
     @Test
     public void testUpdateUserTitleValid() {
-        // Request body
+        System.out.println("\nTest: Melakukan operasi update pada field title dengan data valid\n");
+
+        // Specify base URI
+        RestAssured.baseURI = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
+
+        // Request object (whenever you want to send request to the server)
+        RequestSpecification httpRequest = RestAssured.given();
+
+        // Set request headers
+        httpRequest.header("app-id", "662ba92ffc6498c14d22ae13")
+                .contentType("application/json");
+
+        // Set request body
         String requestBody = "{\"title\": \"mrs\"}";
 
-        given()
-                .contentType(ContentType.JSON)
-                .header("app-id", validAppId)
-                .body(requestBody)
-                .when()
-                .put()
-                .then()
-                .statusCode(200)
-                .body("title", equalTo("mrs"))
-                .log().body()
-                .log().status();
+        // Add request body
+        httpRequest.body(requestBody);
+
+        // Send PUT request and get response
+        Response response = httpRequest.put();
+
+        // Get status code
+        int statusCode = response.getStatusCode();
+
+        // Print response body
+        String responseBody = response.getBody().asString();
+        TestHelper.printPrettyJson(responseBody, statusCode);
+
+        // Validate status code
+        Assert.assertEquals(statusCode, 200, "Status code tidak sesuai dengan yang diharapkan");
+
+        System.out.println("\n==================================================================================================\n");
+
     }
 
+    //TC-03-19-Melakukan operasi update pada field title dengan data invalid (numerik)
     @Test
     public void testUpdateUserTitleInvalid() {
-        // Request body with invalid data (numerical value for title)
+        System.out.println("\nTest: Melakukan operasi update pada field title dengan data invalid (numerik)\n");
+
+        // Specify base URI
+        RestAssured.baseURI = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
+
+        // Request object (whenever you want to send request to the server)
+        RequestSpecification httpRequest = RestAssured.given();
+
+        // Set request headers
+        httpRequest.header("app-id", "662ba92ffc6498c14d22ae13")
+                .contentType("application/json");
+
+        // Set request body
         String requestBody = "{\"title\": 123456}";
 
-        given()
-                .contentType(ContentType.JSON)
-                .header("app-id", validAppId)
-                .body(requestBody)
-                .when()
-                .put()
-                .then()
-                .statusCode(400) // Expected status code
-                .contentType(ContentType.JSON)
-                .body("error", equalTo("BODY_NOT_VALID")) // Expected error message
-                .log().body()
-                .log().status();
+        // Add request body
+        httpRequest.body(requestBody);
+
+        // Send PUT request and get response
+        Response response = httpRequest.put();
+
+        // Get status code
+        int statusCode = response.getStatusCode();
+
+        // Print response body
+        String responseBody = response.getBody().asString();
+        TestHelper.printPrettyJson(responseBody, statusCode);
+
+        // Validate status code
+        Assert.assertEquals(statusCode, 400, "Status code tidak sesuai dengan yang diharapkan");
+
+        System.out.println("\n==================================================================================================\n");
+    }
+
+    //TC-03-46-Melakukan operasi update pada field country dengan data invalid (length < 2)
+    @Test
+    public void testUpdateUserCountryInvalid() {
+        System.out.println("\nTest: Melakukan operasi update pada field country dengan data invalid (length < 2)\n");
+
+        // Specify base URI
+        RestAssured.baseURI = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
+
+        // Request object (whenever you want to send request to the server)
+        RequestSpecification httpRequest = RestAssured.given();
+
+        // Set request headers
+        httpRequest.header("app-id", "662ba92ffc6498c14d22ae13")
+                .contentType("application/json");
+
+        // Set request body
+        String requestBody = "{\n" +
+                "  \"location\": {\n" +
+                "    \"country\": \"j\"\n" +
+                "  }\n" +
+                "}";
+
+        // Add request body
+        httpRequest.body(requestBody);
+
+        // Send PUT request and get response
+        Response response = httpRequest.put();
+
+        // Get status code
+        int statusCode = response.getStatusCode();
+
+        // Print response body
+        String responseBody = response.getBody().asString();
+        TestHelper.printPrettyJson(responseBody, statusCode);
+
+        // Validate status code
+        Assert.assertEquals(statusCode, 400, "Status code tidak sesuai dengan yang diharapkan");
+
+        System.out.println("\n==================================================================================================\n");
+    }
+
+    //TC-03-48-Melakukan operasi update pada field gender dengan data invalid (selain dari opsi "male", "female", "other", "")
+    @Test
+    public void testUpdateUserGenderInvalid() {
+        System.out.println("\nTest: Melakukan operasi update pada field gender dengan data invalid (selain dari opsi male, female, other, string kosong\n");
+
+        // Specify base URI
+        RestAssured.baseURI = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
+
+        // Request object (whenever you want to send request to the server)
+        RequestSpecification httpRequest = RestAssured.given();
+
+        // Set request headers
+        httpRequest.header("app-id", "662ba92ffc6498c14d22ae13")
+                .contentType("application/json");
+
+        // Set request body
+        String requestBody = "{\"gender\": \"unknown\"}";
+
+        // Add request body
+        httpRequest.body(requestBody);
+
+        // Send PUT request and get response
+        Response response = httpRequest.put();
+
+        // Get status code
+        int statusCode = response.getStatusCode();
+
+        // Print response body
+        String responseBody = response.getBody().asString();
+        TestHelper.printPrettyJson(responseBody, statusCode);
+
+        // Validate status code
+        Assert.assertEquals(statusCode, 400, "Status code tidak sesuai dengan yang diharapkan");
+
+        System.out.println("\n==================================================================================================\n");
+    }
+
+    //TC-03-48-Melakukan operasi update pada field email dengan data valid
+    @Test
+    public void testUpdateUserEmailInvalid() {
+        System.out.println("\nTest: Melakukan operasi update pada field email dengan data valid\n");
+
+        // Specify base URI
+        RestAssured.baseURI = "https://dummyapi.io/data/v1/user/60d0fe4f5311236168a109ca";
+
+        // Request object (whenever you want to send request to the server)
+        RequestSpecification httpRequest = RestAssured.given();
+
+        // Set request headers
+        httpRequest.header("app-id", "662ba92ffc6498c14d22ae13")
+                .contentType("application/json");
+
+        // Set request body
+        String requestBody = "{\"email\": \"yasmin@gmail.com\"}";
+
+        // Add request body
+        httpRequest.body(requestBody);
+
+        // Send PUT request and get response
+        Response response = httpRequest.put();
+
+        // Get status code
+        int statusCode = response.getStatusCode();
+
+        // Print response body
+        String responseBody = response.getBody().asString();
+        TestHelper.printPrettyJson(responseBody, statusCode);
+
+        // Validate status code
+        Assert.assertEquals(statusCode, 400, "Status code tidak sesuai dengan yang diharapkan");
+
+        System.out.println("\n==================================================================================================\n");
     }
 }
+
+
+
